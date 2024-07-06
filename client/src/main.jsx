d2834c67551405e4ac5e2c2a05a1e4b3e14c07e6
panditshivammishra <109903290+panditshivammishra@users.noreply.github.com>
2024-07-07 01:03:33 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,13 +14,13 @@ import theme from './theme';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
+//665826723970-mdd1kahmi7300dhsnpqu2eja4ehhau52.apps.googleusercontent.com
 
   <ChakraProvider>
   <BrowserRouter>
      <ChatProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <GoogleOAuthProvider clientId="665826723970-mdd1kahmi7300dhsnpqu2eja4ehhau52.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId="665826723970-5cngs0uee03tcku1si1vqi5n97923k9p.apps.googleusercontent.com">
     <App />
   </GoogleOAuthProvider>
       </ChatProvider>
