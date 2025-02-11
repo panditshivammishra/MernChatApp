548fcf227ccecd1b2087335f3ff5002b7bf762e9
panditshivammishra <panditshivammishra04@gmail.com>
2025-02-11 19:22:58 +0530

    bg-color-change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import Home from "./Pages/Home";
-import { Routes, Route } from 'react-router-dom';
-import Chatpage from './Pages/Chatpage';
+import { Routes, Route } from "react-router-dom";
+import { useColorMode } from "@chakra-ui/react";
+import Chatpage from "./Pages/Chatpage";
 const App = () => {
+  const { colorMode } = useColorMode();
+  useEffect(() => {
+    if (colorMode === "light") {
+      document.body.style.backgroundColor = "#caeae9";
+    }
+  }, [colorMode]);
   return (
     <>
-          <Routes>
-              <Route path="/" element={<Home/>}></Route>
-              <Route path="/chat" element={<Chatpage/>}></Route>
-          </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/chat" element={<Chatpage />}></Route>
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-  
-export default App
+export default App;
